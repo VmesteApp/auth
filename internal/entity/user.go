a7609d87a9416f700326d512149f987a3766a4b8
your Name <qwertyshurazh@gmail.com>
2024-08-29 03:10:25 +0300

    add role field

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -6,6 +6,14 @@ type User struct {
 	ID       int64  `json:"id"`
 	Email    string `json:"email"`
 	PassHash string
+	Role     Role `json:"role"`
 }
+type Role string
+
+const (
+	UserRole Role = "user"
+	AdminRole Role = "admin"
+	SuperAdminRole Role = "superadmin"
+)
 
 var ErrUserExists = errors.New("user exists")
